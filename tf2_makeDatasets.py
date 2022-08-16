0b549ab52592a83b1c59fae89a72ddc37c3d2515
GabrieleCuni <gabriele.cuni@gmail.com>
2022-08-16 17:53:01 +0200

    The last hope seems to work well with alpha = 1.0 but very bad with alpha=0.25

--- tf2_makeDatasets.py
+++ tf2_makeDatasets.py
@@ -11,7 +11,7 @@ def getDatasetWithKeras(imageSize, quantSize, validationSize):
 
     path = os.path.join("calibration_images", "val")    
 
-    dataset = tf.keras.utils.image_dataset_from_directory(
+    quantDataset = tf.keras.utils.image_dataset_from_directory(
                     path,
                     labels='inferred',
                     label_mode='int',
@@ -20,38 +20,56 @@ def getDatasetWithKeras(imageSize, quantSize, validationSize):
                     batch_size=None,
                     image_size=(imageSize, imageSize),
                     shuffle=True,
-                    seed=None,
-                    validation_split=None,
-                    subset=None,
+                    seed=42,
+                    validation_split=0.5,
+                    subset="training",
                     interpolation='bilinear',
                     follow_links=False,
                     crop_to_aspect_ratio=False
                 )
 
+    validationDataset = tf.keras.utils.image_dataset_from_directory(
+                path,
+                labels='inferred',
+                label_mode='int',
+                class_names=None,
+                color_mode='rgb',
+                batch_size=None,
+                image_size=(imageSize, imageSize),
+                shuffle=True,
+                seed=42,
+                validation_split=0.5,
+                subset="validation",
+                interpolation='bilinear',
+                follow_links=False,
+                crop_to_aspect_ratio=False
+            )
 
-    quantDataset = dataset.take(quantSize) 
-    dataset = dataset.skip(quantSize)
-    validationDataset = dataset.take(validationSize)
+
+    # quantDataset = dataset.take(quantSize) 
+    # dataset = dataset.skip(quantSize)
+    # validationDataset = dataset.take(validationSize)
     t1 = time.time()
     print(f"Stop make dataset with Keras. Time: {t1-t0}")
 
-    return quantDataset, validationDataset
+    return quantDataset.take(quantSize), validationDataset.take(validationSize)
 
 
-def makeTfRecord(output_filename, dataset):
+def makeTfRecord(output_filename, dataset, imageSize):
     print("Start write TFrecord")
     count = 0
     with tf.io.TFRecordWriter(output_filename) as writer:
         for x,y in dataset:
+            if count == 0:
+                print(x.dtype)
             count += 1
-            # if count == 0:
-            #     print("raw image: ", x)
-            #     print("label: ", y)
-            #     count = 1
+            
             label_features = tf.train.Feature(int64_list=tf.train.Int64List(value=[int(y)]))
+            imageSize_features = tf.train.Feature(int64_list=tf.train.Int64List(value=[int(imageSize)]))
             image_feature = tf.train.Feature(bytes_list=tf.train.BytesList(value=[tf.io.serialize_tensor(x).numpy()]))
             mapping = {
                 "label": label_features,
+                "imageSize": imageSize_features,
                 "image_raw": image_feature
             }
             tf_example = tf.train.Example(features=tf.train.Features(feature=mapping))
@@ -68,22 +86,21 @@ def main():
     parser.add_argument("-s", "--imageSize", type=int, default=224, choices=imageSizeChoices, help="Default: 224")
     parser.add_argument("-o", "--output", type=str, default="dataset.tfrecord")
     parser.add_argument("--quantSize", type=int, default=1024)
-    parser.add_argument("--validationSize", type=int, default=1024)
+    parser.add_argument("--validationSize", type=int, default=4096)
     args = parser.parse_args()
 
     quantDataset, validationDataset = getDatasetWithKeras(args.imageSize, args.quantSize, args.validationSize)
 
-    makeTfRecord(args.output, quantDataset)
+    datasetPath = os.path.join("tf2_datasets")
 
-    # quantDaasetPath = os.path.join("tf2_datasets","quantization")
-    # validationDatasetPath =  os.path.join("tf2_datasets","validation")
+    if not os.path.exists(datasetPath):
+        os.makedirs(datasetPath)
 
-    # if not os.path.exists(quantDaasetPath):
-    #     os.makedirs(quantDaasetPath)
+    path = os.path.join(datasetPath, f"quantization_{args.imageSize}.tfrecord")
+    makeTfRecord(path, quantDataset, args.imageSize)
 
-    # if not os.path.exists(validationDatasetPath):
-    #     os.makedirs(validationDatasetPath)
-
+    path = os.path.join(datasetPath, f"validation_{args.imageSize}.tfrecord")
+    makeTfRecord(path, validationDataset, args.imageSize)
 
 if __name__ == "__main__":
     main()
---
+++ tf2_vai_flow2.py
@@ -0,0 +1,241 @@
+import os 
+import argparse
+from random import seed
+import time
+import sys
+
+import numpy as np
+from myUtils import DatasetGenerator
+import tensorflow as tf
+from tensorflow_model_optimization.quantization.keras import vitis_quantize
+
+"""
+./docker_run.sh xilinx/vitis-ai:1.3.411
+
+Attention to 1/31 [..............................] - ETA: 0sKilled that means the RAM is not enougth
+"""
+
+def getDataset(path, imageSize, start, stop):
+    # datasetPath = os.path.join(path, f"dataset_{imageSize}_{start}_{stop}")
+    # if not os.path.exists(datasetPath):
+    t0 = time.time()
+    print("\nStart make dataset")
+    print(path)
+    print(f"Start image index: {start}")
+    print(f"Stop image index: {stop}")
+    print(f"imageSize: {imageSize}")
+    datasetGenerator = DatasetGenerator(batch_size=32, startImageNumber=start, stopImageNumber=stop, width=imageSize, height=imageSize)
+    batchedDataset = datasetGenerator.make_dataset()
+    # print(f"Number of images: {len(batchedDataset)}")
+    # print(f"Dataset spec: {batchedDataset.element_spec}") # (TensorSpec(shape=(32,), dtype=tf.float32, name=None), TensorSpec(shape=(32,), dtype=tf.float32, name=None))
+    t1 = time.time()
+    print(f"Stop make dataset. Time: {t1-t0}")
+    # print("Saving dataset on disk")
+    # tf.data.experimental.save(batchedDataset, datasetPath)
+    # t1 = time.time()
+    # print(f"Dataset saved on disk. Time: {t1-t0}")
+    
+    # else:
+    #     print("Loading Dataset from disk")
+    #     t0 = time.time()
+    #     batchedDataset = tf.data.experimental.load(datasetPath, element_spec=(tf.TensorSpec(shape=[32,imageSize,imageSize,3], dtype=tf.float32), tf.TensorSpec(shape=[32,1], dtype=tf.float32)))
+    #     print(f"Number of images: {len(batchedDataset)}")
+    #     t1 = time.time()
+    #     print(f"Dataset loaded. Time: {t1-t0}")
+
+    return batchedDataset
+
+def normalization(x, y):
+    x /= 127.5
+    x -= 1.
+    return x, y
+
+
+
+def quantization(model, preprocessQuantDataPath, alpha, imageSize, start, stop):
+    # batchedQuantDataset = getDataset(preprocessQuantDataPath, imageSize, start, stop)
+    batchedQuantDataset = getTFdataset(imageSize, "quantization")
+
+    print("Start Quantization")
+    t0 = time.time()
+    quantizer = vitis_quantize.VitisQuantizer(model)
+    quantized_model = quantizer.quantize_model(calib_dataset=batchedQuantDataset)
+    quantized_model.save(os.path.join("tf2_vai_quant_models",f"quantized_mobilenet_{alpha}_{imageSize}.h5"))
+    t1 = time.time()
+    print(f"Stop Quantization. Time: {t1-t0}")
+
+def validation(preprocessValDataPath, alpha, imageSize, start, stop):
+    # batchedValidationDataset = getDataset(preprocessValDataPath, imageSize, start, stop)
+    batchedValidationDataset = getTFdataset(imageSize, "validation")
+
+    print("Start Validation of the quantized model")
+    t0 = time.time()
+    with vitis_quantize.quantize_scope():
+        modelPath = os.path.join("tf2_vai_quant_models",f"quantized_mobilenet_{alpha}_{imageSize}.h5")
+        quantized_model = tf.keras.models.load_model(modelPath, compile=False)
+
+    quantized_model.compile(	
+        loss=tf.keras.losses.SparseCategoricalCrossentropy(),
+        metrics= [tf.keras.metrics.SparseCategoricalAccuracy(), tf.keras.metrics.SparseTopKCategoricalAccuracy(k=5)]) 
+    print("Validation accuracy:")
+    quantized_model.evaluate(batchedValidationDataset, verbose=2)
+    t1 = time.time()
+    print(f"Stop Validation. Time: {t1-t0}")
+
+def validateOriginalTf2Model(model, preprocessValDataPath, alpha, imageSize, start, stop):
+    batchedValidationDataset = getDataset(preprocessValDataPath, imageSize, start, stop)
+    model.compile(	
+        loss=tf.keras.losses.SparseCategoricalCrossentropy(),
+        metrics= [tf.keras.metrics.SparseCategoricalAccuracy(), tf.keras.metrics.SparseTopKCategoricalAccuracy(k=5)]) 
+    print("Start validate original tf2 model")
+    print("Validation accuracy:")
+    t0 = time.time()
+    model.evaluate(batchedValidationDataset, verbose=2)
+    t1 = time.time()
+    print(f"Stop Validation. Time: {t1-t0}")
+
+def _parser_fn(record):
+    name_to_features = {
+        'label': tf.io.FixedLenFeature([], tf.int64),
+        'imageSize': tf.io.FixedLenFeature([], tf.int64),
+        'image_raw': tf.io.FixedLenFeature([], tf.string),
+    }
+
+    return tf.io.parse_single_example(record, name_to_features)
+
+def _decode_fn(record):
+    # image = tf.io.decode_raw(record['image_raw'], out_type=tf.float32)
+    image = tf.io.parse_tensor(record['image_raw'], out_type=tf.float32)
+    label = record['label']
+    dimension = record['imageSize']
+    image = tf.reshape(image, (dimension, dimension, 3))
+    return (image, label)
+
+
+
+def getTFdataset(imageSize, subset):
+    dataset = tf.data.TFRecordDataset(os.path.join("tf2_datasets", f"{subset}_{imageSize}.tfrecord"))
+    dataset = dataset.map(_parser_fn)
+    dataset = dataset.map(_decode_fn)
+    dataset = dataset.map(normalization)
+    batchedDataset = dataset.batch(32, drop_remainder=True)
+
+    return batchedDataset
+
+
+def validateOriginalTf2Model_withTf2utilsPreprocess(alpha, imageSize, start, stop):
+    # i = tf.keras.layers.Input([None, None, 3], dtype = tf.uint8)
+    # x = tf.cast(i, tf.float32)
+    # x = tf.keras.applications.mobilenet.preprocess_input(x)
+    # core = tf.keras.applications.MobileNet(alpha=alpha, input_shape=(imageSize,imageSize,3))
+    # x = core(x)
+    # model = tf.keras.Model(inputs=[i], outputs=[x])
+
+    # datasetGenerator = DatasetGenerator(batch_size=32, startImageNumber=start, stopImageNumber=stop, width=imageSize, height=imageSize)
+    # batchedDataset = datasetGenerator.make_dataset_without_preprocessing()
+
+    batchedDataset = getTFdataset(imageSize, "validation")
+
+    model = tf.keras.applications.MobileNet(alpha=alpha, input_shape=(imageSize,imageSize,3))
+
+    model.compile(	
+        loss=tf.keras.losses.SparseCategoricalCrossentropy(),
+        metrics= [tf.keras.metrics.SparseCategoricalAccuracy(), tf.keras.metrics.SparseTopKCategoricalAccuracy(k=5)]) 
+    print("Start validate original tf2 model")
+    print("Validation accuracy:")
+    t0 = time.time()
+    model.evaluate(batchedDataset, verbose=2)
+    t1 = time.time()
+    print(f"Stop Validation. Time: {t1-t0}")
+
+def compiler(dpu, alpha, imageSize):
+    outputPath = os.path.join("tf2_vai_compiled_models", dpu, f"tf2_mobilenet_v1_{alpha}_{imageSize}_{dpu}")
+    quantModelPath = os.path.join("tf2_vai_quant_models",f"quantized_mobilenet_{alpha}_{imageSize}.h5")
+    archPath = os.path.join("Arch_files", f"arch_{dpu}.json")
+
+    if os.path.exists(outputPath) is False:
+        os.makedirs(outputPath)
+
+    
+    shell_command = f"vai_c_tensorflow2 \
+                        -m {quantModelPath} \
+                        -a {archPath} \
+                        -o {outputPath} \
+                        -n tf2mobilenet_v1_{alpha}_{imageSize}_{dpu}"   
+
+    stream = os.popen(shell_command)
+    output = stream.read()
+    print(output)
+    
+
+def main():
+    alphaChoices = [1.0, 0.75, 0.5, 0.25]
+    imageSizeChoices = [224, 192, 160, 128]
+    dpuChoices = ["B4096", "B3136", "B2304", "B1600", "B1152", "B1024", "B800", "B512"]
+    tfModelsPath = os.path.join("tf_models")
+    preprocessValDataPath = os.path.join("tf2_preprocessDatasets", "validationDatasets")
+    preprocessQuantDataPath = os.path.join("tf2_preprocessDatasets", "quantizationDatasets")
+
+    if not os.path.exists(tfModelsPath):
+        os.mkdir(tfModelsPath)
+
+    if not os.path.exists(preprocessValDataPath):
+        os.makedirs(preprocessValDataPath)
+
+    if not os.path.exists(preprocessQuantDataPath):
+        os.makedirs(preprocessQuantDataPath)
+    
+
+    parser = argparse.ArgumentParser()
+    parser.add_argument("-a", "--alpha", type=float, default=1.0, choices=alphaChoices, help="Default: 1.0")	
+    parser.add_argument("-s", "--imageSize", type=int, default=224, choices=imageSizeChoices, help="Default: 224")
+    parser.add_argument("--startQuant", type=int, default=0)
+    parser.add_argument("--stopQuant", type=int, default=1024)
+    parser.add_argument("--startVal", type=int, default=1024)
+    parser.add_argument("--stopVal", type=int, default=2048)
+    parser.add_argument("-d", "--dpu", type=str, default="B4096",choices=dpuChoices , help="Default: B4096")
+    parser.add_argument("-v", "--verbose", action='store_true')
+    parser.add_argument("-q", "--quantize", action='store_true', help="If you want start the quantization -q")
+    parser.add_argument("--validate", action="store_true", help="If you want to validate the quantized model --validate") 
+    parser.add_argument("--validateOriginal", action="store_true", help="If you want to validate the original tf2 model --validateOriginal")
+    parser.add_argument("-c", "--compile", action='store_true', help="If you want start the compilation -q")
+    # parser.add_argument("-o", "--outputLayer", type=str, default="MobilenetV1/Predictions/Reshape") # MobilenetV1/Predictions/Reshape_1
+    args = parser.parse_args()
+
+    print("************************************")
+    print("INPUT PARAMETERS:")
+    print(f"\tmodel: mobilenet_v1_{args.alpha}_{args.imageSize}")
+    print(f"\tDPU: {args.dpu}")
+    # print(f"\tOutput Layer: {args.outputLayer}")
+    print(f"\tExecute quantization: {args.quantize}")
+    print(f"\tExecute compilation: {args.compile}")
+    print("************************************")
+
+    model = tf.keras.applications.MobileNet(alpha=args.alpha, input_shape=(args.imageSize,args.imageSize,3))
+
+    modelPath = os.path.join(tfModelsPath, f"tf2_mobilenet_v1_{args.alpha}_{args.imageSize}")
+    if not os.path.exists(modelPath):
+        model.save(modelPath)
+        model = tf.keras.applications.MobileNet(alpha=args.alpha, input_shape=(args.imageSize,args.imageSize,3))
+    else:
+        model = tf.keras.models.load_model(modelPath, compile=False)
+
+    if args.verbose:
+        print(model.summary())
+
+    if args.quantize:        
+        quantization(model, preprocessQuantDataPath, args.alpha, args.imageSize, args.startQuant, args.stopQuant)
+
+    if args.validate:
+        validation(preprocessValDataPath, args.alpha, args.imageSize, args.startVal, args.stopVal)
+
+    if args.validateOriginal:
+        # validateOriginalTf2Model(model, preprocessValDataPath, args.alpha, args.imageSize, args.startVal, args.stopVal)
+        validateOriginalTf2Model_withTf2utilsPreprocess(args.alpha, args.imageSize, args.startVal, args.stopVal)
+
+    if args.compile:
+        compiler(args.dpu, args.alpha, args.imageSize)
+    
+
+if __name__ == "__main__":
+    main()
