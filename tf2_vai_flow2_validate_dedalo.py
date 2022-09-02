6dfa1978dcd1d4ad254554d348e9dd4102d5c535
GabrieleCuni <gabriele.cuni@gmail.com>
2022-09-02 12:03:33 +0200

    D

--- tf2_vai_flow2_validate_dedalo.py
+++ tf2_vai_flow2_validate_dedalo.py
@@ -3,6 +3,8 @@ import argparse
 import time
 
 import tensorflow as tf
+# from tensorflow_model_optimization.quantization.keras import vitis_quantize
+import tensorflow_model_optimization as tfmot
 
 """
 ./docker_run.sh xilinx/vitis-ai:1.3.411
@@ -11,7 +13,8 @@ Attention to 1/31 [..............................] - ETA: 0sKilled that means th
 """
 
 def getTFdataset(imageSize):
-    path = os.path.join("/","space_fast","imagenet_folder","val")
+    # path = os.path.join("/","space_fast","imagenet_folder","val")
+    path = os.path.join("calibration_images", "val") 
 
     batchedDataset = tf.keras.utils.image_dataset_from_directory(
                 path,
@@ -38,8 +41,11 @@ def validation(alpha, imageSize):
 
     print("Start Validation of the quantized model")
     t0 = time.time()
-    modelPath = os.path.join("tf2_vai_quant_models",f"quantized_mobilenet_{alpha}_{imageSize}.h5")
-    quantized_model = tf.keras.models.load_model(modelPath, compile=False)
+    
+
+    with tfmot.quantization.keras.quantize_scope():
+        modelPath = os.path.join("tf2_vai_quant_models",f"quantized_mobilenet_{alpha}_{imageSize}.h5")
+        quantized_model = tf.keras.models.load_model(modelPath, compile=False)
 
     quantized_model.compile(	
         loss=tf.keras.losses.SparseCategoricalCrossentropy(),
@@ -66,7 +72,6 @@ def validateOriginal(model, imageSize):
 def main():
     alphaChoices = [1.0, 0.75, 0.5, 0.25]
     imageSizeChoices = [224, 192, 160, 128]
-    dpuChoices = ["B4096", "B3136", "B2304", "B1600", "B1152", "B1024", "B800", "B512"]
     tfModelsPath = os.path.join("tf_models")
 
     if not os.path.exists(tfModelsPath):
@@ -75,7 +80,6 @@ def main():
     parser = argparse.ArgumentParser()
     parser.add_argument("-a", "--alpha", type=float, default=1.0, choices=alphaChoices, help="Default: 1.0")	
     parser.add_argument("-s", "--imageSize", type=int, default=224, choices=imageSizeChoices, help="Default: 224")
-    parser.add_argument("-v", "--verbose", action='store_true')
     parser.add_argument("--validate", action="store_true", help="If you want to validate the quantized model --validate") 
     parser.add_argument("--validateOriginal", action="store_true", help="If you want to validate the original tf2 model --validateOriginal")
     args = parser.parse_args()
@@ -83,21 +87,13 @@ def main():
     print("************************************")
     print("INPUT PARAMETERS:")
     print(f"\tmodel: mobilenet_v1_{args.alpha}_{args.imageSize}")
-    print(f"\tVerbose: {args.verbose}")
     print(f"\tExecute original model validation: {args.validateOriginal}")
     print(f"\tExecute quantized model validation: {args.validate}")
     print("************************************")
 
-    # model = tf.keras.applications.MobileNet(alpha=args.alpha, input_shape=(args.imageSize,args.imageSize,3))
-
-    if args.verbose:
-        if model is None:
-            model = tf.keras.applications.MobileNet(alpha=args.alpha, input_shape=(args.imageSize,args.imageSize,3))
-        print(model.summary())
+    model = tf.keras.applications.MobileNet(alpha=args.alpha, input_shape=(args.imageSize,args.imageSize,3))
 
     if args.validateOriginal:
-        if model is None:
-            model = tf.keras.applications.MobileNet(alpha=args.alpha, input_shape=(args.imageSize,args.imageSize,3))
         validateOriginal(model, args.imageSize)
 
 
